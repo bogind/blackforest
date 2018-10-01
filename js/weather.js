b5f2dcc87819016db08403a783d5b4236b259a19
Dror Bogin <Dror.Bogin@gmail.com>
2018-10-01 22:39:22 +0300

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -28,102 +28,32 @@ var cities = {"Freiburg":2925177, "Titisee":2822084, "Todtnau":2821975, "Triberg
 
 
 
-// Create Tile Basemap
-
-var OSM = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
-			{attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'});
-
-// create map object
-
-var map = L.map('map',
-		{center: [47.992008, 8.136182], 
-		zoom: 9.25,
-		layers: [OSM]});
-
-// Empty Variables
-		
-var basemap = {"מפת רקע OpenStreetMap":OSM}
-var blackforest;
-var towns_points;
-var towns_polygon;
-var layers_c;
-/*			
-$.getJSON("data/blackforest.geojson", function(data) {
-	 blackforest = L.geoJSON(data, {
-		 style: function style(feature) {
-						return {
-							weight: 1,
-							opacity: 1,
-							color: "#09c73d",
-							fillOpacity: 0.0
-						};
-					},
-			interactive: false
-	 }).addTo(map)
-	 blackforest.bindTooltip("היער השחור", {className: 'tooltip',direction: "center"}).openTooltip();
-	 return([ blackforest]);
-})
-*/
-
-// popup div for point layer
-
-function popup_point(feature, layer) {
-				if (feature.properties && feature.properties.hebname) {
-				layer.bindPopup(
-								"<h4><b>שם העיר: </b>" + 
-								feature.properties.hebname + 
-								"</h4><span style='direction: rtl;'><b>שם בגרמנית: </b></span><span style='text-align: left;'>" + 
-								feature.properties.name + 
-								"</br></br>" + 
-								"<img src='"+feature.properties.image +"' style='border-radius: 8px; height: 100px; width: 150px;' previewImage=true></img>"+
-								"<br><b>הסבר קצר</b></br>"+feature.properties.description+
-								"</span></br><a href='" + 
-								feature.properties.url + "'>הסבר מורחב</a>"
-								);
+/*	
+$.ajax({
+		type: 'GET',
+		url: url,
+		success: function(resp) {
+		var weather = resp;
+		$("#weather-icon").html("<img src='https://openweathermap.org/img/w/"+weather.list[0].weather[0]["icon"]+".png' width='45' height='45'/>");
+		$("#clouds").html(" Cloud Cover: "+ weather.list[0].weather[0].description + ", " + weather.list[0].clouds["all"]+"%");
+		$("#temp").html(" Temperature: "+ weather.list[0].main.temp + "&#176C" +
+					"<br> Humidity: " + weather.list[0].main.humidity+"%" );
+					if('rain' in weather.list[0] && '3h' in weather.list[0].rain){
+						if(typeof w.list[0].rain["3h"] === "undefined"){
+						$("#rain").html("");
+						}else{
+						$("#rain").html(" Precipitation: "+ weather.list[0].rain["3h"]);
+						}
 					}
-}
-
-$.getJSON("data/towns_point.geojson", function(data) {
-		towns_points = L.geoJSON(data, {
-			pointToLayer: function (feature, latlng) {
-				return L.circleMarker(latlng);
-			},
-			style: function style(feature){
-				return {
-				weight: 1,
-				opacity: 1,
-				color: "#e55203",
-				radius: 5,
-				fillcolor: "#e55203",
-				fillOpacity: 0.75,
-				}
-			},
-			onEachFeature: popup_point
-			}).addTo(map);
-			
-		
-			
-		
 					
-			 return([ towns_points]);
-			 
-}).then(function(){
-	$.getJSON("data/towns_polygon.geojson",function(data){
-			towns_polygon = L.geoJSON(data, {
-			style : function style(feature){
-				return {
-					color: "#109da0",
-					opacity: 0.85,
-					fillOpacity: 0
-				}
-			},
-			interactive: false
-			}).addTo(map);
-			
-			 return([ towns_polygon]);
-		})
-}).then(function(){
-	
+				  },
+				  error: function() {
+				   console.log('error')
+				  }
+				});
+
+
+
 var weatherurl = "https://api.openweathermap.org/data/2.5/forecast?id="+cities["Freiburg"]+"&units=metric&appid="+apikey
 $.get(weatherurl,function(resp){
    weather = resp;
@@ -143,7 +73,6 @@ $.get(weatherurl,function(resp){
 															"טמפרטורה: "+
 															weather.list[0].main.temp + "&#176C"+"</br>"+
 															"<div id='chart' class='c3' style='max-height: 280px; position: relative;'>");
-	
 	return Freiburg
 	}
 ).then(function(){
@@ -213,7 +142,7 @@ var weatherurl = "https://api.openweathermap.org/data/2.5/forecast?id="+cities["
 	var hours = date1.getHours().toString()
 	var Minutes = date1.getMinutes.toString()
 	var date2 = null
-	Triberg = L.marker([lat,lng], {icon: curIcon}).bindPopup("<h4>מזג האוויר ב: "+"Triberg".capitalize()+"</h4>"+
+	Triberg = L.marker([lat,lng], {icon: curIcon}).bindPopup("<h4>מזג האוויר ב: "+city_name.capitalize()+"</h4>"+
 															"<span>נכון לתאריך ושעה:"+
 															weather.list[0].dt_txt+"</br>"+
 															"טמפרטורה: "+
@@ -276,6 +205,31 @@ var weatherurl = "https://api.openweathermap.org/data/2.5/forecast?id="+cities["
 	
 }).then(function(){
 
+var weatherurl = "https://api.openweathermap.org/data/2.5/forecast?id="+cities["Bad Wildbad"]+"&units=metric&appid="+apikey
+	$.get(weatherurl,function(resp){
+   weather = resp;
+   return weather;
+}).then(function(){
+	lat = weather.city.coord["lat"];
+	lng = weather.city.coord["lon"];
+	iconurl = "./js/weathericons/"+weather.list[0].weather[0].icon+".png";
+	var curIcon = new WeatherIcon({iconUrl: iconurl });
+	var date1 = new Date(weather.list[0].dt_txt)
+	var hours = date1.getHours().toString()
+	var Minutes = date1.getMinutes.toString()
+	var date2 = null
+	Bad_Wildbad = L.marker([lat,lng], {icon: curIcon}).bindPopup("<h4>מזג האוויר ב: "+"Bad Wildbad".capitalize()+"</h4>"+
+															"<span>נכון לתאריך ושעה:"+
+															weather.list[0].dt_txt+"</br>"+
+															"טמפרטורה: "+
+															weather.list[0].main.temp + "&#176C"+"</br>"+
+															"<div id='chart' class='c3' style='max-height: 280px; position: relative;'>");
+	return Bad_Wildbad
+	}
+)
+	
+}).then(function(){
+
 
 var weatherurl = "https://api.openweathermap.org/data/2.5/forecast?id="+cities["Rust"]+"&units=metric&appid="+apikey
 	$.get(weatherurl,function(resp){
@@ -301,51 +255,7 @@ var weatherurl = "https://api.openweathermap.org/data/2.5/forecast?id="+cities["
 )
 	
 }).then(function(){
-	$.wait(2000).then(function(){
-		var citiesW = L.layerGroup([Freiburg, Titisee, Todtnau, Triberg, Baden_Baden, Basel, Rust]);
-	
-	layers_c = {"מרכזי ערים":towns_points,
-					"גבולות עירוניים":towns_polygon,
-					"מזג אוויר": citiesW};
-	L.control.layers(basemap,layers_c).addTo(map);
-	return layers_c
-	})
-	
-	
+	var citiesW = L.layerGroup([Freiburg, Titisee, Todtnau, Triberg, Baden_Baden, Basel, Bad_Wildbad, Rust]);
+	return citiesW;
 })
-
-	
-})
-
-//.then(function(){
-//$.wait(5000).then(function(){
-
-//});	
-	
-	//return([ layers_c]);
-//});
-
-
-
-// add search bar using OSM
-
-var osmGeocoder = new L.Control.OSMGeocoder({
-					collapsed: true,
-					position: 'topright',
-					text: 'חיפוש',
-				});
-osmGeocoder.addTo(map);
-
-
-
-/*
-var chart = c3.generate({
-																		data: {
-																			x: 'x',
-																			columns: [
-																				['x', 30, 50, 100, 230, 300, 310],
-																				['data1', 30, 200, 100, 400, 150, 250],
-																				['data2', 130, 300, 200, 300, 250, 450]
-																			]
-																		}
-																	});*/
+*/
